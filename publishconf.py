0e355d96286f34a312944c97538c93f0f6f643d9
hamub <hbutau35@gmail.com>
2018-01-03 19:33:56 +0200

    added disqus

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "https-pyzim-github-io"
+# DISQUS_SITENAME = "https-pyzim-github-io"
 #GOOGLE_ANALYTICS = ""
