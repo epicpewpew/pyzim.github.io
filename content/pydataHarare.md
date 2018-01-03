7399ac3f7f06d0c98b58ed6aea40e1f9672bd721
hamub <hbutau35@gmail.com>
2018-01-03 16:11:05 +0200

    initial commit

---
+++ content/pydataHarare.md
@@ -0,0 +1,7 @@
+Title: PyData Harare 
+Date: 2018-02-02 10:20
+Category: datascience 
+
+Django Indaba will be the first serious gathering of Djangonauts in Zimbabwe. We will be meeting for talk, workshops, and fun all for the love 
+of python s most loved web framework.
+
---
+++ pelicanconf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Humphrey Butau'
+SITENAME = 'pyzim'
+SITEURL = ''
+THEME = 'html5-dopetrope'
+THEME_STATIC_DIR = 'theme'
+
+PATH = 'content'
+
+TIMEZONE = 'Africa/Harare'
+
+DEFAULT_LANG = 'en'
+
+MENUITEMS = (
+    ('Django Indaba', '/'),
+#    ('Schedule', '/schedule'),
+    ('Donate', '/sponsorship'),
+#    ('Call for Proposals', '/call_for_proposals'),
+    ('Volunteering', '/call_for_volunteers'),
+    ('CoC', '/coc'),
+    ('Recent Developments', '/#developments'),
+#    ('About', '/about'),
+)
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+ABOUT_TEXT = 'Whats this aldjkdfksjkasjfkjasdkasl about?'
+ABOUT_IMAGE = 'theme/images/pic10.jpg'
+MAIL = 'pyzim@gmail.com'
+PHONE = 'Your phone number.'
+TWITTER_USER  = 'pycon_zim'
+GOOGLEPLUS_USER = '110831175850960549188'
+ABOUT_LINK = 'about'
+FACEBOOK_USER = 'pyzim'
+LINKEDIN_USER = 'pyzim'
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = [
+    'theme/images',
+    'theme/js',
+    'theme/css',
+    'extra/robots.txt',
+    'README.md',
+]
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
